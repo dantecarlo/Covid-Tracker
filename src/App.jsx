438bc3c3a418997c8fedc8da008b5ec9a2758d1d
dantecarlo <dante.cnp@gmail.com>
2020-05-06 01:47:13 +0100

    Add Google Analitycs tracker

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React, { useState, useEffect } from 'react'
+import ReactGA from 'react-ga'
+import { createBrowserHistory } from 'history'
+import { Router } from 'react-router-dom'
 
 import { Cards, Chart, CountryPicker } from './components'
 import styles from './App.module.css'
 import { fetchData } from './api'
 
 import coronaImage from './images/image.png'
+
+ReactGA.initialize(process.env.REACT_APP_GAID)
+
+const history = createBrowserHistory()
+
+history.listen(location => {
+  ReactGA.set({ page: location.pathname })
+  ReactGA.pageview(location.pathname)
+})
 
 const App = () => {
   const [covidData, setCovidData] = useState({})
@@ -25,12 +37,14 @@ const App = () => {
   }
 
   return (
-    <div className={styles.container}>
-      <img src={coronaImage} className={styles.image} alt="COVID-19" />
-      <Cards covidData={covidData} />
-      <CountryPicker handleCountryChange={handleCountryChange} />
-      <Chart covidData={covidData} country={country} />
-    </div>
+    <Router history={history}>
+      <div className={styles.container}>
+        <img src={coronaImage} className={styles.image} alt="COVID-19" />
+        <Cards covidData={covidData} />
+        <CountryPicker handleCountryChange={handleCountryChange} />
+        <Chart covidData={covidData} country={country} />
+      </div>
+    </Router>
   )
 }
 
